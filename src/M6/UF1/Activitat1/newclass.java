eb890bf180af6b8b353fc6c8798f29488318d421
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-01-12 20:02:52 +0100

    1

---
+++ src/M6/UF1/Activitat1/newclass.java
@@ -0,0 +1,28 @@
+//1
+//Accepti de línia d'ordres (args[0]) el directori o fitxer que es vol consultar. Cal comprova que existeix file.exists()
+//Si és un directori (file.isDirectory()) que mostri el seu contingut, com ho fa a CODI1.1
+//Si és un fitxer (file.isFile()) que mostri la seva informació, com ho fa a CODI1.2
+
+//2
+//Es creen objectes tipus Cotxes: marca, model, any i matricula.
+//Pugui guardar i recuperar objectes tipus Cotxe com un bloc.
+//Pugui guardar objectes tipus Cotxe d'un en un.
+//Pugui recuperar objectes tipus Cotxe segons un camp.
+
+//3
+
+
+//4
+
+//5
+
+//6
+package Java;
+
+/**
+ *
+ * @author alexp
+ */
+public class newclass {
+    
+}
