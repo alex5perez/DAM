2c70910556922d9c6a4d024ecec5bbfb5b77f516
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-01-26 17:08:18 +0100

    commit

---
+++ src/M9/UF2/Activitat4/apartat3.java
@@ -0,0 +1,15 @@
+/*
+Heu d’implementar de forma recursiva (1) i mitjançant fils (2, 
+patró Fork-Join) amb la classe RecursiveTask (2) dos dels següents algoritmes:
+ */
+package M9.UF2.Activitat4;
+
+/*
+* File: Exercici9UF2M9.java 
+* Author: Alex Pérez Rubio i Hector Garzon Borras
+* Date: 26-01-2021
+* Description: Activitat 4 PRACTICA UF2 M9
+*/
+public class apartat3 {
+    
+}
