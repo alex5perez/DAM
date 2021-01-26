86cf360a479e984e33feb8b282b1783069e6d572
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-01-26 17:53:01 +0100

    Comentaris i fi

--- src/M9/UF2/Activitat4/apartat1.java
+++ src/M9/UF2/Activitat4/apartat1.java
@@ -15,6 +15,7 @@ import java.util.concurrent.*;
 public class apartat1 {
     
     static class Caixa implements Runnable {
+        //Num client i el temps del articles
         private int[] temps = {2, 3, 4, 5, 6, 7, 8};
         private int numClient;
         
@@ -24,21 +25,26 @@ public class apartat1 {
 
         @Override
         public void run() {
+            //Numero d'articles aleatori
             int articlesRandom = (int) (Math.random() * (30 - 1 + 1)) + 1;
             int tempsRandom;
             
+            //Mostrem els clients i els seus numeros d'article a comprar
             System.out.println("Creat Client " + numClient + " amb " + articlesRandom + " articles.");
             System.out.println("Client " + numClient + " passa per caixa ");
             for (int j = 1; j < articlesRandom; j++) {
                 tempsRandom = (int) (Math.random() * (6 - 0 + 1));
                 try {
+                    //Temps parara
                     Thread.sleep(temps[tempsRandom] * 1000);
                 }catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 if (j == articlesRandom) {
+                    //Si el client te tots els articles sen va
                     System.out.println("Client " + numClient + " article " + j + "/" + articlesRandom + " (" + temps[tempsRandom] + " segons) FINALITZAT");
                 }else{
+                    //Si no, segueix
                     System.out.println("Client " + numClient + " article " + j + "/" + articlesRandom + " (" + temps[tempsRandom] + " segons)");
                 }
             }
@@ -47,9 +53,11 @@ public class apartat1 {
     }
     
     public static void main(String[] args) throws InterruptedException, ExecutionException {
+        //Fils
         ScheduledThreadPoolExecutor executor = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool (10);
         
-        for (int i = 1; i <= 500; i++) {
+        //Numero de clients
+        for (int i = 1; i <= 50; i++) {
             Caixa task = new Caixa(i);
             executor.scheduleWithFixedDelay(task, 0 , 3, TimeUnit.SECONDS);
     }
