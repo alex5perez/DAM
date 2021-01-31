6854548e9321f9de549ff3537fbedfcdebba2ab2
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-01-31 22:15:54 +0100

    .

--- src/M9/UF2/Activitat4/apartat2.java
+++ src/M9/UF2/Activitat4/apartat2.java
@@ -5,6 +5,10 @@ fan durant l’execució.
  */
 package M9.UF2.Activitat4;
 
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 /*
 * File: Exercici9UF2M9.java 
 * Author: Alex Pérez Rubio i Hector Garzon Borras
@@ -19,8 +23,28 @@ public class apartat2 {
         
         int[] numRandom = new int[20];
         int numeros = (9 - 1);
+        int grupAux;
                 
-        NumeroBombolla[] numerosBombolla = NumeroBombolla[4];
+        NumeroBombolla[] numerosBombolla = new NumeroBombolla[4];
+        
+        for (int i = 0; i < numRandom.length; i++) {
+            numRandom[i] = (int) (Math.random() * numeros) + 1;
+        }
+
+        System.out.println(Arrays.toString(numRandom));
+
+        int contadorAux = 0;
+        for (int i = 0; i < numerosBombolla.length; i++) {
+            int[] arrayAux = new int[numRandom.length / 4];
+            for (int j = 0; j < arrayAux.length; j++) {
+                arrayAux[j] = numRandom[contadorAux];
+                contadorAux++;
+            }
+            NumeroBombolla arrayBomb = new  NumeroBombolla(arrayAux, i);
+            numerosBombolla[i] = arrayBomb;
+        }
+
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
         
         
     }
