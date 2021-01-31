933fae566c30ef22c3174381686a1de912ecb633
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-01-31 21:56:19 +0100

    .

---
+++ src/M9/UF2/Activitat4/NumeroBombolla.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package M9.UF2.Activitat4;
+
+/**
+ *
+ * @author alexp
+ */
+public class NumeroBombolla implements Runnable{
+    
+    int[] array;
+    int grup;
+    
+    public NumeroBombolla(int[] numerosBombolla, int grup) {
+        
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
