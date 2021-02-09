f124fdc6e1caf3678243aaaa7e8af2574204d228
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-02-09 13:02:22 +0100

    .

---
+++ src/M6/UF2/Activitat7/GuestListener.java
@@ -0,0 +1,33 @@
+//Segon tutorial amb GlassFish
+package M6.UF2.Activitat7;
+  /*
+import javax.persistence.*;
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebListener;
+
+* File: Exercici6UF2M6.java 
+* Author: Alex Pérez Rubio
+* Date: 3-11-2020
+* Description: Activitat 7 PRACTICA UF2 M6
+
+//@WebListener
+//public class GuestListener implements ServletContextListener {
+
+    // Prepare the EntityManagerFactory & Enhance:
+    //@Override
+    //public void contextInitialized(ServletContextEvent e) {
+        //com.objectdb.Enhancer.enhance("guest.*");
+        EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("$objectdb/db/guest.odb");
+        e.getServletContext().setAttribute("emf", emf);
+    }
+ 
+    // Release the EntityManagerFactory:
+    @Override
+    public void contextDestroyed(ServletContextEvent e) {
+        EntityManagerFactory emf =
+            (EntityManagerFactory)e.getServletContext().getAttribute("emf");
+        emf.close();
+    }
+}
+*/
