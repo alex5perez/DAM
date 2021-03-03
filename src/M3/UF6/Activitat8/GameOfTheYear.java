1673eb03372f827f13c0d4ef48e94a33a98089ff
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-03-03 20:22:09 +0100

    ho he borrat sense volar, pero encara estaba al github

--- src/M3/UF6/Activitat8/GameOfTheYear.java
+++ src/M3/UF6/Activitat8/GameOfTheYear.java
@@ -1,5 +1,7 @@
 /*
-Game of The Year
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package M3.UF6.Activitat8;
 
