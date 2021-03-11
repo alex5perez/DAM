a9cc10742b5cf591dad96b0b354e9033b3689a91
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-03-11 16:20:20 +0100

    .

---
+++ src/M6/UF3/Activitat3/activitat3.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package M6.UF3.Activitat3;
+
+import org.xmldb.api.*;
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.XPathQueryService;
+
+/**
+ *
+ * @author alexp
+ */
+public class activitat3 {
+    public static void main (String[] args) throws XMLDBException {
+        String driver = "org.exist.xmldb.Databaselmpl"; //Driver para eXist
+        
+        Collection col = null; // Colección
+        
+        String URI="xmldb:exist://localhost:8080/exist/xmlrpc/db/Pruebas"; //URI colección
+        String usu="admin"; //Usuario
+        
+        String usuPwd="admin"; //Clave
+        
+        try {
+            Class cl = Class.forName(driver); //Cargar del driver
+   
+            Database database = (Database) cl.newInstance(); //Instancia de la BD 
+            
+            DatabaseManager.registerDatabase(database); //Registro del driver
+        } catch (Exception e) {
+            System.out.println("Error al inicializar la BD eXist");
+             e.printStackTrace(); 
+        }
+        
+        col = DatabaseManager.getCollection(URI, usu, usuPwd);
+        if(col == null)
+            System.out.println(" *** LA COLECCION NO EXISTE. ***");
+        
+        XPathQueryService servicio = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+        
+        ResourceSet result = servicio.query ("for $em in /EMPLEADOS/EMP_ROW[DEPT_NO=10] return $em");
+        // recorrer los datos del recurso.
+        ResourceIterator i;
+        i = result.getIterator();
+        if (!i.hasMoreResources())
+            System.out.println(" LA CONSULTA NO DEVUELVE NADA."); 
+        while (i.hasMoreResources()) {
+            Resource r = i.nextResource();
+            System.out.println((String) r.getContent());
+        }
+        col.close(); //borramos
+    }// FIN verempleados10
+}
