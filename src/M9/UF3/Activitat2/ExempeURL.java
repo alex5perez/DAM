dd23b91693a3e42624f37f58f16a9064d67e61af
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-03-16 19:55:07 +0100

    2

---
+++ src/M9/UF3/Activitat2/ExempeURL.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package M9.UF3.Activitat2;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author alexp
+ */
+public class ExempeURL {
+    
+    public static void main (String[] args) {
+		URL url;
+		
+	String direccio = args[0];
+        int port = Integer.parseInt(args[1]);
+        
+            try {
+                url = new URL("http", direccio, port, "/");
+                Visualitzar (url);
+            } catch (MalformedURLException e) { System.out.println(e); }
+                
+            
+
+	}
+    
+        private static void Visualitzar(URL url) {
+		
+		BufferedReader in;
+		
+		try {
+			
+			//Url
+			InputStream inputStream = url.openStream();
+			in = new BufferedReader(new InputStreamReader(inputStream));
+			
+			String inputLine;
+			//Mostra per pantalla per linea
+			while ((inputLine = in.readLine()) != null)
+				System.out.println(inputLine);
+			in.close();
+			
+		} catch (IOException e) {e.printStackTrace(); }
+
+		
+	}
+
+}
+
+
