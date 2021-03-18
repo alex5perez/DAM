9823a3e928e3f10d50c05eab126362e31e501615
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-03-18 17:39:03 +0100

    while

--- src/M9/UF3/Activitat4/ClientUDP3.java
+++ src/M9/UF3/Activitat4/ClientUDP3.java
@@ -29,27 +29,33 @@ public class ClientUDP3 {
 		InetAddress IPServidor = InetAddress.getLocalHost();
 		int port = 9800;
 		
-		//INTRODUIR DADES PEL TECLAT
-		System.out.print("Introdueix missatge: ");
-		String cadena = in.readLine();
-		enviats = cadena.getBytes();
+                while(clientSocket.getSoTimeout() <= 5000){
+                    
+                    //INTRODUIR DADES PEL TECLAT
+                    System.out.print("Introdueix missatge: ");
+                    String cadena = in.readLine();
+                    enviats = cadena.getBytes();
+                    
+                    //ENVIANT DATAGRAMA AL SERVIDOR
+                    System.out.println("Enviant "+enviats.length+"bytes al servidor.");
+                    DatagramPacket enviament = new DatagramPacket(enviats, enviats.length, IPServidor, port);
+                    clientSocket.send(enviament);
+
+                    //REBENT DATAGRAMA DEL SERVIDOR
+                    DatagramPacket rebut = new DatagramPacket(rebuts, rebuts.length);
+                    System.out.println("Esperant datagrama...");
+                    clientSocket.receive(rebut);
+                    String majuscula = new String(rebut.getData());
+
+                    //ACONSEGUINT INFORMACIÓ DEL DATAGRAMA
+                    InetAddress IPOrigen = rebut.getAddress();
+                    int portOrigen = rebut.getPort();
+                    System.out.println("\tProcedent de: "+IPOrigen+":"+portOrigen);
+                    System.out.println("\tDades: "+majuscula.trim());
+                }
 		
-		//ENVIANT DATAGRAMA AL SERVIDOR
-		System.out.println("Enviant "+enviats.length+"bytes al servidor.");
-		DatagramPacket enviament = new DatagramPacket(enviats, enviats.length, IPServidor, port);
-		clientSocket.send(enviament);
 		
-		//REBENT DATAGRAMA DEL SERVIDOR
-		DatagramPacket rebut = new DatagramPacket(rebuts, rebuts.length);
-		System.out.println("Esperant datagrama...");
-		clientSocket.receive(rebut);
-		String majuscula = new String(rebut.getData());
 		
-		//ACONSEGUINT INFORMACIÓ DEL DATAGRAMA
-		InetAddress IPOrigen = rebut.getAddress();
-		int portOrigen = rebut.getPort();
-		System.out.println("\tProcedent de: "+IPOrigen+":"+portOrigen);
-		System.out.println("\tDades: "+majuscula.trim());
 		//Tanca el socket
 		clientSocket.close();
 		
