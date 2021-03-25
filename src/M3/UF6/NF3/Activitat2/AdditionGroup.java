ac6180c33fa581db3279f6a447e01b74060bb284
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-03-25 20:58:51 +0100

    .

---
+++ src/M3/UF6/NF3/Activitat2/AdditionGroup.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package M3.UF6.NF3.Activitat2;
+
+/**
+ *
+ * @author alexp
+ */
+public class AdditionGroup {
+    
+}
