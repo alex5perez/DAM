a251ee9fc81127adf7e790229441442f838b02f7
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-03-26 12:59:21 +0100

    .

---
+++ src/M3/UF6/NF2/Activitat1/FractalTester.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package M3.UF6.NF2.Activitat1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alexp
+ */
+public class FractalTester {
+    public static void main(String[] args) {
+        Scanner teclat = new Scanner (System.in);
+        
+        System.out.println("Longitut del quadat mes gran: ");
+        int costat = teclat.nextInt();
+        int costatGran = costat;
+        int quadrats = 1;
+        int perimetreTotal = 0;
+        
+        while(costatGran >= 1){
+            
+        }
+    }
+}
