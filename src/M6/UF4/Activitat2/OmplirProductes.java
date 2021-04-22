161adfb7e0277cec4aaffd9b8c7e317134486969
alex5perez <71727357+alex5perez@users.noreply.github.com>
2021-04-22 15:49:58 +0200

    .

--- src/M6/UF4/Activitat2/OmplirProductes.java
+++ src/M6/UF4/Activitat2/OmplirProductes.java
@@ -5,6 +5,7 @@ package M6.UF4.Activitat2;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import ElsMeusBeans.Comanda;
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
 import org.neodatis.odb.Objects;
@@ -19,25 +20,24 @@ import ElsMeusBeans.Producte;
 public class OmplirProductes {
     public static void main (String[] args) {
         //Obrir la base de dades
-        ODB odb = ODBFactory.open("Producte_com.BD");
-        Producte p1 = new Producte(1, "Barcelona, Una biografia", 10, 3, 160);
-        Producte p2 = new Producte(2, "La magia de l'ordre", 5, 2, 176);
-        Producte p3 = new Producte(3, "Tot es al teu cap", 20, 5, 193);
-        Producte p4 = new Producte(4, "Filosofía inacabada", 8, 3, 85);
-        Producte p5 = new Producte(5, "La resistència íntima", 7, 1, 159);
-        Producte p6 = new Producte(6, "El capital al segle xxi", 5, 2, 80);
+        Producte[] productes = {
+        new Producte(1, "Barcelona, Una biografia", 10, 3, 160),
+        new Producte(2, "La magia de l'ordre", 5, 2, 176),
+        new Producte(3, "Tot es al teu cap", 20, 5, 193),
+        new Producte(4, "Filosofía inacabada", 8, 3, 85),
+        new Producte(5, "La resistència íntima", 7, 1, 159),
+        new Producte(6, "El capital al segle xxi", 5, 2, 80)};
         
-        //S'emmagatzema els productes
-        odb.store(p1);
-        odb.store(p2);
-        odb.store(p3);
-        odb.store(p4);
-        odb.store(p5);
-        odb.store(p6);
-        
-        //Es tanca la base de dades
-        odb.close();
+            for (int i=0; i<productes.length; i++){
+
+
+            Comanda comanda = new Comanda();
+
+            productes[i].addPropertyChangeListener(comanda);
+            //Es canvia l'estoc actual, se li dona valor 2
+            productes[i].setStockactual(2);
+            
+            }
     }
-    
 }
 
